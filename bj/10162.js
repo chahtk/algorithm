7764944987ee7da2e6762811df0e4bca4e181e4e
chahtk <chahtk@gmail.com>
2021-01-08 06:34:33 +0900

    solv

---
+++ bj/10162.js
@@ -0,0 +1,32 @@
+const readline = require("readline");
+
+const solve = (T) => {
+  const buttons = [300, 60, 10];
+  let t = T;
+  let result = "";
+
+  buttons.map((btn) => {
+    result += `${Math.floor(t / btn)} `;
+    t %= btn;
+  });
+
+  return t === 0 ? result : -1;
+};
+
+const inputData = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on("line", (line) => {
+    const T = +line;
+    const answer = solve(T);
+    console.log(answer);
+    rl.close();
+  }).on("close", () => {
+    process.exit();
+  });
+};
+
+inputData();
