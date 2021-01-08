f13fa9c326273d06603da545c1c664c1250982ec
chahtk <chahtk@gmail.com>
2021-01-08 10:16:17 +0900

    solve 나중에 다시 풀것

--- bj/10164.js
+++ bj/10164.js
@@ -1,52 +1,33 @@
 const readline = require("readline");
 
-const findPath = (start, end, path) => {
-  const dx = [0, 1]; // 행
-  const dy = [1, 0]; // 열
-  let countPathO = 0;
+const getXYofO = (m, o) => {
+  const x = Math.floor((o - 1) / m);
+  const y = (o - 1) % m;
+  return [x, y];
+};
 
-  const que = [];
-  que.push(start);
-
-  while (que.length) {
-    const [x, y] = que.shift();
-    if (x === end[0] && y === end[1]) {
-      countPathO += 1;
-      continue;
-    }
-
-    for (let i = 0; i < 2; i += 1) {
-      const ndx = x + dx[i];
-      const ndy = y + dy[i];
-      if (ndx <= end[0] && ndy <= end[1]) {
-        que.push([ndx, ndy]);
-      }
+const findPath = (board, start, end) => {
+  for (let i = start[0]; i <= end[0]; i += 1) {
+    for (let j = start[1]; j <= end[1]; j += 1) {
+      if (i === start[0] || j === start[1]) board[i][j] = 1;
+      else board[i][j] = board[i - 1][j] + board[i][j - 1];
     }
   }
-  return countPathO;
+  return board[end[0]][end[1]];
 };
 
 const solve = ([n, m, o]) => {
-  let cnt = 1;
-  let ox = 0;
-  let oy = 0;
-  const board = Array.from(Array(n), () => new Array(m));
-  const path = Array.from(Array(n), () => Array(m).fill(0));
-  for (let i = 0; i < n; i += 1) {
-    for (let j = 0; j < m; j += 1) {
-      board[i][j] = cnt;
-      if (cnt === o) {
-        ox = i;
-        oy = j;
-      }
-      cnt += 1;
-    }
-  }
+  const board = Array.from(Array(n), () => Array(m).fill(0));
+  let answer = 1;
+  let x = 0;
+  let y = 0;
 
-  let countPathO = 1;
-  if (o !== 0) countPathO = findPath([0, 0], [ox, oy], path);
-  const countPathX = findPath([ox, oy], [n - 1, m - 1], path);
-  return countPathO * countPathX;
+  if (o !== 0) {
+    [x, y] = getXYofO(m, o);
+    answer = findPath(board, [0, 0], [x, y]);
+  }
+  answer *= findPath(board, [x, y], [n - 1, m - 1]);
+  return answer;
 };
 
 const inputData = () => {
