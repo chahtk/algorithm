d8425536aed3ab1ddfbe20c2e39ff954852ae01c
chahtk <chahtk@gmail.com>
2021-08-14 13:22:19 +0900

    add rule "no bitwise"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-const OS = require("os");
+const OS = require('os');
 
 module.exports = {
   env: {
@@ -7,19 +7,20 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
   },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   rules: {
-    "prefer-const": 0,
-    "prefer-destructuring": 0,
-    "no-console": 0,
-    "consistent-return": 0,
-    "no-unused-vars": 0,
-    "no-param-reassign": 0,
-    "no-constant-condition": 0,
-    "array-callback-return": 0,
-    "no-continue": 0,
-    "linebreak-style": ["error", OS.EOL === "\r\n" ? "windows" : "unix"],
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    'prefer-const': 0,
+    'prefer-destructuring': 0,
+    'no-console': 0,
+    'consistent-return': 0,
+    'no-unused-vars': 0,
+    'no-param-reassign': 0,
+    'no-constant-condition': 0,
+    'array-callback-return': 0,
+    'no-continue': 0,
+    'no-bitwise': 0,
+    'linebreak-style': ['error', OS.EOL === '\r\n' ? 'windows' : 'unix'],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
-  ignorePatterns: ["node_modules/"],
+  ignorePatterns: ['node_modules/'],
 };
